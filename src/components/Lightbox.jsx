ba31efbfe98339e3a2bee095b4324ede5560d66e
aocheng <aocheng>
2026-07-08 19:46:38 +0200

    Zoom en lightbox

--- src/components/Lightbox.jsx
+++ src/components/Lightbox.jsx
@@ -1,4 +1,5 @@
 import Lightbox from "yet-another-react-lightbox";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
 import "yet-another-react-lightbox/styles.css";
 
 export default function ImageLightbox({ images, index, open, onClose }) {
@@ -8,6 +9,18 @@ export default function ImageLightbox({ images, index, open, onClose }) {
       close={onClose}
       index={index}
       slides={images.map((src) => ({ src }))}
+      plugins={[Zoom]}
+      zoom={{
+        maxZoomPixelRatio: 5,
+        zoomInMultiplier: 2,
+        doubleTapDelay: 300,
+        doubleClickDelay: 300,
+        doubleClickMaxStops: 2,
+        keyboardMoveDistance: 50,
+        wheelZoomDistanceFactor: 100,
+        pinchZoomDistanceFactor: 100,
+        scrollToZoom: true,
+      }}
     />
   );
 }
